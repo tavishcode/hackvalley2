0d76f068150ceae3b68e40999f2f297df712adc6
zalsader <zalsader@hotmail.com>
2018-02-24 18:25:33 -0500

    Edit emoji list

--- convert.py
+++ convert.py
@@ -3,13 +3,14 @@ import json
 emojifile = open('emoji.json', 'r')
 emojilist = json.loads(emojifile.read())
 emojidict = {}
+ignored_keys = ['fruit', 'food']
 for emoji in emojilist:
     if 'emoji' not in emoji:
         continue
-    keywords = emoji.get('aliases', []) + emoji.get('tags', [])
-    if 'description' in emoji:
-        keywords.append(emoji['description'])
+    keywords = emoji.get('aliases', []) + emoji.get('tags', []) + emoji.get('description', '').split()
     for keyword in keywords:
+        if keyword in ignored_keys:
+            continue
         emojidict[keyword] = emojidict.get(keyword, []) + [emoji['emoji']]
 emojioutfile = open('emojiconv.py', 'w')
-emojioutfile.write(repr(emojidict))
+emojioutfile.write('object_emoji=' + repr(emojidict))
